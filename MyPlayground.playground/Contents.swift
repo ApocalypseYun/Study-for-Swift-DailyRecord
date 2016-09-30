f05e12be1b7defa127b5625f532389e50ba7b2b0
ApocalypseYun <Apocalypse@liuguanzhongdeMacBook-Air.local>
2016-09-30 17:31:43 +0800

    9.30

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,48 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempt = 0
+print(str,".",maximumNumberOfLoginAttempts)
+var x=0.0,y=0.0,z=0.0
+x=x+1
+print(x)
+var welcomemessage:String
+welcomemessage="Hello"
+print(welcomemessage)
+var biubiubiu="Apocalypse"
+str=str+"。"+welcomemessage+", "+biubiubiu
+print(str)
+var red, green, blue: Double
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dogcow"
+print(你好,🐶🐮)
+let label = "The width is"
+let width = 94
+let widthLabel = label+" "+String(width)
+print(widthLabel)
+var shoppingList = ["catfish", "water", "tulips", "blue paint"]
+shoppingList[1] = "bottle of water"
+print(shoppingList)
+var occupations = [ "Malcolm": "Captain", "Kaylee": "Mechanic", ]
+occupations["Jayne"] = "Public Relations"
+print(occupations)
+var empty=[String]()
+shoppingList = []
+occupations = [:]
+let individualScores = [75, 43, 103, 87, 12]
+var teamScore = 0
+for llll in individualScores {
+    if llll > 50 {
+    
+    teamScore += 3
+    }
+    else {
+    
+    teamScore += 1
+    }
+}
+print(teamScore)
